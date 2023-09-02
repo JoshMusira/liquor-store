cefb427b7478b42a389e25cd7cf5b434ee6500c9
JoshMusira <joshuamusira01@gmail.com>
2023-09-03 01:12:03 +0300

    create layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { Navbar } from './exports/exports'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1 className='text-3xl font-bold underline'>Liquor Store</h1>
     </>
   )
