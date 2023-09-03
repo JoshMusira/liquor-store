05808a3aa30c14cb779f4792a90756f4fa1fef3d
JoshMusira <joshuamusira01@gmail.com>
2023-09-03 19:29:32 +0300

    feat: create navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,29 @@
+import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div>
+            {/* Left Side  */}
+            <div>
+                <div>
+                    <AiOutlineMenu />
+                </div>
+                <h1>Liquor Store</h1>
+                <div>
+                    <p>Delivery</p>
+                    <p>Pickup</p>
+                </div>
+
+            </div>
+
+            {/* Search Input */}
+            <div>
+                <AiOutlineSearch />
+                <input type="text" placeholder='Search drinks...' />
+            </div>
+
+
+        </div>
     )
 }
 
