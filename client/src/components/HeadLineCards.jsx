9467b19cf66c0415e70dceb09b214a108592a696
JoshMusira <joshuamusira01@gmail.com>
2023-09-06 19:48:49 +0300

    feat: create mobile and hero page

--- client/src/components/HeadLineCards.jsx
+++ client/src/components/HeadLineCards.jsx
@@ -11,6 +11,8 @@ const HeadLineCards = () => {
             <HeadLineCard />
             <HeadLineCard />
             <HeadLineCard />
+            <HeadLineCard />
+            <HeadLineCard />
 
         </div>
     )
