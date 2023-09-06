f46b480cc2b42e100a29289bd93a8658de32cd05
JoshMusira <joshuamusira01@gmail.com>
2023-09-07 01:24:13 +0300

    feat:update main menu

--- client/src/components/MainDrinks.jsx
+++ client/src/components/MainDrinks.jsx
@@ -24,9 +24,19 @@ const MainDrinks = () => {
                     </div>
                 </div>
             </div>
-            {/* <div>
+            <div className=" w-[200px] sm:w-[450px] lg:w-[500px] grid grid-cols-2 lg:grid-cols4 gap-6 pt-4">
+                <div className="border shadow-lg cursor-pointer rounded-lg hover:scale-105 duration-300">
+                    <img className="w-full h-[345px] object-cover rounded-t-lg" src="https://images.unsplash.com/photo-1569529465841-dfecdab7503b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YWxjb2hvbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                    <div className=" flex text-2xl px-2 py-3 justify-between">
+                        <p className="font-bold ">Black Label</p>
+                        <p >
+                            <span className=" bg-orange-500 text-white p-1 rounded-[10px]">$$$
+                            </span>
+                        </p>
+                    </div>
+                </div>
 
-            </div> */}
+            </div>
         </div >
     )
 }
